e5411cce4ba6727015edf696bef446849e46a66a
SrDevO <76437512+SrDaveo28@users.noreply.github.com>
2022-05-25 23:05:58 -0400

    basic crud of images

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -1,20 +1,64 @@
 import { Request, Response } from 'express'
+import Photo from '../models/Photo'
+import path from 'path'
+import fs from 'fs-extra'
 
 export function health (req: Request, res: Response): Response {
   return res.send('Server Running')
 }
 
-export function createPhoto (req: Request, res: Response) {
-  const { body } = req
-  console.log(body)
+export async function createPhoto (req: Request, res: Response): Promise<Response> {
+  const { title, description } = req.body
+  const { file } = req
+  const newPhoto = { title, description, imagePath: file?.path }
+
+  const photo = new Photo(newPhoto)
+
+  await photo.save()
 
   return res.json({
-    message: 'Photo successfully saved'
+    message: 'Photo successfully saved',
+    photo
   })
 }
 
-export function getPhotos (req: Request, res: Response) {
+export async function getPhotos (req: Request, res: Response): Promise<Response> {
+  const photos = await Photo.find()
+
+  return res.json(photos)
+}
+
+export async function getPhoto (req: Request, res: Response): Promise<Response> {
+  const { id } = req.params
+  const photo = await Photo.findById(id)
+
+  return res.json(photo)
+}
+
+export async function deletePhoto (req: Request, res: Response): Promise<Response> {
+  const { id } = req.params
+
+  const photo = await Photo.findByIdAndDelete(id)
+
+  if (photo) {
+    fs.unlink(path.resolve(photo.imagePath))
+  }
+
   return res.json({
-    message: 'Photo successfully saved'
+    message: 'Photo successfully deleted',
+    photo
+  })
+}
+
+export async function updatePhoto (req: Request, res: Response): Promise<Response> {
+  const { id } = req.params
+  const { title, description } = req.body
+  const updatedPhoto = await Photo.findByIdAndUpdate(id, {
+    title,
+    description
+  })
+  return res.json({
+    message: 'Successfully updated',
+    updatedPhoto
   })
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
-import { health, createPhoto, getPhotos } from '../controllers/photo.controller'
+import { health, createPhoto, getPhotos, getPhoto, deletePhoto, updatePhoto } from '../controllers/photo.controller'
 import multer from '../libs/multer'
 
 const router = Router()
 
 router.route('/health').get(health)
 
-router.route('/create')
-  .post(multer.single('image'), createPhoto)
-  .get(getPhotos)
+router.route('/create').post(multer.single('image'), createPhoto)
+
+router.route('/photos').get(getPhotos)
+
+router.route('/photos/:id').get(getPhoto)
+
+router.route('/photos/:id').delete(deletePhoto)
+
+router.route('/photos/:id').put(updatePhoto)
 
 export default router
